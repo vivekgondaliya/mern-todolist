3790774b08245796783418dbe58896730065df71
Vivek Gondalia <vivekgondalia@Viveks-MacBook-Air.local>
2023-08-17 17:26:32 -0700

    connecting to Mongo AtlasDB

--- api/server.js
+++ api/server.js
@@ -4,5 +4,14 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(exress.json());
+app.use(express.json());
 app.use(cors());
+
+mongoose.connect("mongodb+srv://vgondalia:Password123@mern-cluster.q5uedbb.mongodb.net/", {
+    useNewUrlParser : true,
+    useUnifiedTopology: true
+})
+    .then(()=> console.log("Connected to DB"))
+    .catch(console.error);
+
+app.listen(3001, () => console.log("Server started on port 3001"));
