10d32adbb7e49378f7ee74be53ade8c9fb77caeb
Vivek Gondalia <vivekgondalia@Viveks-MacBook-Air.local>
2023-08-17 17:51:50 -0700

    a POST route to create new TODO item

--- api/server.js
+++ api/server.js
@@ -16,10 +16,21 @@ mongoose.connect("mongodb+srv://vgondalia:Password123@mern-cluster.q5uedbb.mongo
 
 const Todo = require('./models/Todo');
 
+//GET ALL TODOS
 app.get('/todos', async (req, res) => {
     const todos = await Todo.find();
 
     res.json(todos);
-})
+});
+
+//CREATE A NEW TODO
+app.post('/todo/new', (req, res) => {
+    const todo = new Todo({
+        text : req.body.text
+    });
+
+    todo.save();
+    res.json(todo);
+});
 
 app.listen(3001, () => console.log("Server started on port 3001"));
