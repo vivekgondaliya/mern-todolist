be4700f2364ad4cb063de55b62504166051b0b82
Vivek Gondalia <vivekgondalia@Viveks-MacBook-Air.local>
2023-08-22 08:03:31 -0700

    html for todo

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,19 @@
 function App() {
 	return (
 		<div className="App">
-			
+			<h1>Welcome, Vivek</h1>
+			<h4>Your Tasks</h4>
+
+			<div className="todos">
+				<div className="todo">
+					<div className="checkbox"></div>
+
+					<div className="text">Code and Coffee</div>
+					
+					<div className="delete-todo">x</div>
+				</div>
+
+			</div>
 		</div>
 	); 
 }
