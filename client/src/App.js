a172fcdf348d09eef4b5fffe53c8b9d2f9c71452
Vivek Gondalia <vivekgondalia@Viveks-MacBook-Air.local>
2023-08-23 08:11:51 -0700

    set up state to store todos

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,24 @@
+import {useState, useEffect} from 'react';
+const API_BASE = "http://localhost:3001";
+
 function App() {
+
+	const [todos, setTodos] = useState([]);
+	const [popupActive, setPopupActive] = useState(false);
+	const [newTodo, setNewTodo] = useState("");
+
+	useEffect(() => {
+		GetTodos();
+		console.log(todos);
+	}, []);
+
+	const GetTodos = () => {
+		fetch(API_BASE + '/todos')
+			.then(res => res.json())
+			.then(data => setTodos(data))
+			.catch(err => console.error("Error: ", err));
+	}
+
 	return (
 		<div className="App">
 			<h1>Welcome, Vivek</h1>
